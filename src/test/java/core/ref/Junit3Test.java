86372886636629ba67fa9a6e59c5b108272d85ed
t1t1 <cha@hanmail2.net>
2016-08-01 20:55:32 +0900

    #Junit3에서 test로 시작하는 메소드를 단위 테스트로 실행하고 싶다.

--- src/test/java/core/ref/Junit3Test.java
+++ src/test/java/core/ref/Junit3Test.java
@@ -1,6 +1,16 @@
 package core.ref;
 
+import static org.junit.Assert.*;
+
+import java.lang.reflect.Method;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Junit3Test {
+	private static final Logger log = LoggerFactory.getLogger(Junit3Test.class);
+	
 	public void test1() throws Exception {
 		System.out.println("Running Test1");
 	}
@@ -11,5 +21,30 @@ public class Junit3Test {
 	
 	public void three() throws Exception {
 		System.out.println("Running Test3");
+	}
+	
+	public Junit3Test() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Test
+	public void testTestMethods() throws Exception {
+		Class<Junit3Test> clazz = Junit3Test.class;
+		clazz.newInstance();
+		
+//		Method[] methods = clazz.getMethods();
+		Method[] methods = clazz.getDeclaredMethods();
+		for (Method method : methods) {
+			log.debug("method.getName(): {}", method.getName()); // TODO; delete;
+			String methodName = method.getName();
+			if (methodName.startsWith("test")) {
+				if (methodName.startsWith("testTest")) {
+					continue;
+				}
+				log.debug("method.getName(): {}: invoke", method.getName()); // TODO; delete;
+				method.invoke(clazz.newInstance());
+			}
+		}
+		
 	}
 }
