7d65971ae412b2347e14a9bc7b88570fed771e74
t1t1 <cha@hanmail2.net>
2016-08-01 20:54:14 +0900

    #Question 클래스의 모든 필드, 생성자, 메소드를 출력한다.

--- src/test/java/core/ref/ReflectionTest.java
+++ src/test/java/core/ref/ReflectionTest.java
@@ -1,5 +1,9 @@
 package core.ref;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,11 +11,28 @@ import org.slf4j.LoggerFactory;
 import next.model.Question;
 
 public class ReflectionTest {
-	private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
+	private static final Logger log = LoggerFactory.getLogger(ReflectionTest.class);
 	
 	@Test
 	public void showClass() {
 		Class<Question> clazz = Question.class;
-		logger.debug(clazz.getName());
+		log.debug(clazz.getName());
+		
+//		Field[] fields = clazz.getFields();
+		Field[] fields = clazz.getDeclaredFields();
+		for (Field field : fields) {
+			log.debug("field.getName(): {}", field.getName()); // TODO; delete;
+		}
+		
+		Method[] methods = clazz.getDeclaredMethods();
+		for (Method method : methods) {
+			log.debug("method.getName(): {}", method.getName()); // TODO; delete;
+		}
+		
+		Constructor[] constructors = clazz.getDeclaredConstructors();
+		for (Constructor constructor : constructors) {
+			log.debug("constructor.getName(): {}", constructor.getName()); // TODO; delete;
+		}
+		
 	}
 }
