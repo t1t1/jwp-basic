f958648e1c4612de1def2718ffb7790dae3d978b
t1t1 <cha@hanmail2.net>
2016-08-01 21:05:37 +0900

    #Junit4에서 Test 어노테이션과 같이 @MyTest 애노테이션이 있는 메서드를 실행한다.

--- src/test/java/core/ref/Junit4TestRunner.java
+++ src/test/java/core/ref/Junit4TestRunner.java
@@ -1,11 +1,31 @@
 package core.ref;
 
+import java.lang.reflect.Method;
+
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Junit4TestRunner {
+	private static final Logger log = LoggerFactory.getLogger(Junit4TestRunner.class);
+	
 	@Test
 	public void run() throws Exception {
 		Class<Junit4Test> clazz = Junit4Test.class;
+		
+//		Method[] methods = clazz.getMethods();
+		Method[] methods = clazz.getDeclaredMethods();
+		for (Method method : methods) {
+			log.debug("method.getName(): {}", method.getName()); // TODO; delete
+			log.debug("method.isAnnotationPresent(MyTest.class): {}", method.isAnnotationPresent(MyTest.class)); // TODO; delete;
+			if (method.isAnnotationPresent(MyTest.class)) {
+//				method.invoke(clazz);
+//				method.invoke(Junit4Test.class);
+				method.invoke(Junit4Test.class.newInstance());
+			}
+		}
+		
+		
 
 	}
 }
