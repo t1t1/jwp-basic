63d5f27b8aecaab31a793404207b620b58ce4b2c
t1t1 <cha@hanmail2.net>
2016-08-08 21:21:15 +0900

    #answersNotExist; success

--- src/test/java/next/service/QnaServiceTest.java
+++ src/test/java/next/service/QnaServiceTest.java
@@ -7,6 +7,7 @@ import static org.mockito.Mockito.*;
 import java.util.Date;
 
 import next.CannotDeleteException;
+import next.dao.AnswerDao;
 import next.dao.QuestionDao;
 import next.model.Question;
 import next.model.User;
@@ -24,26 +25,37 @@ public class QnaServiceTest {
 	
 	@Mock
 	private QuestionDao questionDao;
+	
+	@Mock
+	private AnswerDao answerDao;
 
 	@InjectMocks
 	private QnaService qnaService;
 	
 	@Test(expected=CannotDeleteException.class)
 	public void test_questionIsNull() throws CannotDeleteException {
-		Question question = new Question(0001L, "writer", "title", "contents", new Date(), 1);
+		Question question = new Question(0001L, "userId", "title", "contents", new Date(), 1);
 		User user = new User("userId", "password", "name", "email");
-		when(questionDao.findById(question.getQuestionId())).thenReturn(null);
+		when(questionDao.findById(anyLong())).thenReturn(null);
 		qnaService.deleteQuestion(question.getQuestionId(), user);
 	}
 	
 	@Test(expected=CannotDeleteException.class)
 	public void test_youAreNotWriter() throws Exception {
-		Question question = new Question(0001L, "writer", "title", "contents", new Date(), 1);
+		Question question = new Question(0001L, "userId", "title", "contents", new Date(), 1);
 		User user = new User("userId", "password", "name", "email");
 		when(question.isSameUser(user)).thenReturn(false); // fail
 		qnaService.deleteQuestion(question.getQuestionId(), user);
 	}
 	
+	@Test
+	public void test_answersNotExist() throws Exception {
+		Question question = new Question(0001L, "userId", "title", "contents", new Date(), 1);
+		User user = new User("userId", "password", "name", "email");
+		when(answerDao.findAllByQuestionId(anyLong())).thenReturn(null);
+		questionDao.delete(question.getQuestionId());
+		assertNull(questionDao.findById(question.getQuestionId()));
+	}
 	
 
 }
