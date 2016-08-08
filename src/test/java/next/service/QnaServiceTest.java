3c6d5ce87f306e54af9b1f396d7c29f8b69f5154
t1t1 <cha@hanmail2.net>
2016-08-08 21:00:53 +0900

    #JUnit #Mockito #expeted exception

---
+++ src/test/java/next/service/QnaServiceTest.java
@@ -0,0 +1,39 @@
+package next.service;
+
+import static org.hamcrest. CoreMatchers.*;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Date;
+
+import next.CannotDeleteException;
+import next.dao.QuestionDao;
+import next.model.Question;
+import next.model.User;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class QnaServiceTest {
+	
+	@Mock
+	private QuestionDao questionDao;
+
+	@InjectMocks
+	private QnaService qnaService;
+	
+	@Test(expected=CannotDeleteException.class)
+	public void test_questionIsNull() throws CannotDeleteException {
+		Question question = new Question(0001L, "writer", "title", "contents", new Date(), 1);
+		User user = new User("userId", "password", "name", "email");
+		when(questionDao.findById(question.getQuestionId())).thenReturn(null);
+		qnaService.deleteQuestion(question.getQuestionId(), user);
+	}
+
+}
